fbf7c1ad39ab62cea836cebd882e946749720632
BeamSol <141314118+BeamSol@users.noreply.github.com>
2025-08-18 21:10:11 +0300

    Created 'Relative Sort Array' by A2SV Hub

---
+++ 18-Aug-2025/Relative Sort Array 386580.py
@@ -0,0 +1,22 @@
+# Problem: Relative Sort Array - https://leetcode.com/problems/relative-sort-array/description/
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        cnt = defaultdict(int)
+        set_ = set()
+        for num in arr2:
+            for n in arr1:
+                if num == n:
+                    cnt[num]+=1
+                    set_.add(num)
+        ans= []
+        for key,val in cnt.items():
+            for i in range(val):
+                ans.append(key)
+        temp = []
+        for n in arr1:
+            if n not in set_:
+                temp.append(n)
+        temp = sorted(temp)
+        ans.extend(temp)
+        return ans
