c7a27aa08fd5118a2306bd49e605dac0b8842eb4
BeamSol <141314118+BeamSol@users.noreply.github.com>
2025-08-31 21:39:34 +0300

    Created 'Number of smaller' by A2SV Hub

---
+++ 31-Aug-2025/Number of smaller 390451.py
@@ -0,0 +1,13 @@
+# Problem: Number of smaller - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/B
+
+n, m = map(int, input().split())
+list1 = list(map(int, input().split()))
+list2 = list(map(int, input().split()))
+
+ans = []
+right = 0
+for i in range(m):
+    while right < n and list1[right] < list2[i]:
+        right += 1
+    ans.append(right)
+print(*ans)
