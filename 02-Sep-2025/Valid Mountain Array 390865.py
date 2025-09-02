0584d49c8ee35ee5b66a92d9da56defa992e714d
BeamSol <141314118+BeamSol@users.noreply.github.com>
2025-09-02 20:28:27 +0300

    Created 'Valid Mountain Array' by A2SV Hub

---
+++ 02-Sep-2025/Valid Mountain Array 390865.py
@@ -0,0 +1,13 @@
+# Problem: Valid Mountain Array - https://leetcode.com/problems/valid-mountain-array/description/
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        i, j, n = 0, len(arr) - 1, len(arr)
+    
+        while i + 1 < n and arr[i] < arr[i + 1]:
+            i += 1
+
+        while j > 0 and arr[j - 1] > arr[j]:
+            j -= 1
+
+        return 0 < i == j < n - 1
