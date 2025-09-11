9ded29393ae032838ff109a28af3111f0aaa458e
BeamSol <141314118+BeamSol@users.noreply.github.com>
2025-09-11 20:22:08 +0300

    Created 'Mod Power' by A2SV Hub

---
+++ 11-Sep-2025/Mod Power 392614.py
@@ -0,0 +1,10 @@
+# Problem: Mod Power - https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())  # Input for a
+b = int(input())  # Input for b
+m = int(input())  # Input for m
+
+print(pow(a, b))
+
+print(pow(a, b, m))
