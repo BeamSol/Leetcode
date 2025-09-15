a3847362d2b82aa9afd211794d6bfdcd8be2fffc
BeamSol <141314118+BeamSol@users.noreply.github.com>
2025-09-15 20:56:54 +0300

    Created 'Three Divisors' by A2SV Hub

---
+++ 15-Sep-2025/Three Divisors 393315.py
@@ -0,0 +1,11 @@
+# Problem: Three Divisors - https://leetcode.com/problems/three-divisors/description/?envType=problem-list-v2&envId=number-theory
+
+class Solution:
+    def isThree(self, n: int) -> bool:
+        s=set()
+        for i in range(1, n+1):
+            if n%i==0:
+                s.add(i)
+        if len(s) == 3:
+            return True
+        return False
