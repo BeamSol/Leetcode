1daa986c1cc392be685ebc1f827eedcc20101917
Beamlak Solomon <141314118+BeamSol@users.noreply.github.com>
2025-10-17 11:00:39 +0300

    Created 'Complicated GCD' by A2SV Hub

---
+++ 17-Oct-2025/Complicated GCD 403931.py
@@ -0,0 +1,34 @@
+# Problem: Complicated GCD - https://codeforces.com/contest/664/problem/A
+
+from collections import defaultdict, Counter, deque
+import math, sys, heapq
+import threading
+from sys import stdin, stdout, setrecursionlimit
+from heapq import heapify, heappop, heappushpop, heappush
+
+# setrecursionlimit(1 << 30)
+# threading.stack_size(1 << 27)  # Uncomment if recursion depth needed
+
+input = lambda: sys.stdin.readline().strip()
+number = lambda: int(sys.stdin.readline().strip())
+numbers = lambda: map(int, sys.stdin.readline().strip().split())
+string = lambda: sys.stdin.readline().strip()
+strings = lambda: input().split()
+array = lambda: list(map(int, sys.stdin.readline().strip().split()))
+array_set = lambda: set(map(int, sys.stdin.readline().strip().split()))
+yn = lambda x: 'YES' if x else 'NO'
+
+
+def solve():
+    a, b = numbers()
+    if b-a >= 1:
+        print(1)
+    else:
+        print(a)
+
+def main():
+    t = 1
+    for _ in range(t):
+        solve()
+
+main()
