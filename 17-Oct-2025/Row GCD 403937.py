3635edc6057502b589c6e689ea0a53301b867bb5
Beamlak Solomon <141314118+BeamSol@users.noreply.github.com>
2025-10-17 11:35:45 +0300

    Created 'Row GCD' by A2SV Hub

---
+++ 17-Oct-2025/Row GCD 403937.py
@@ -0,0 +1,42 @@
+# Problem: Row GCD - https://codeforces.com/problemset/problem/1458/a
+
+from collections import defaultdict, Counter, deque
+import math, sys, heapq
+import threading
+from sys import stdin, stdout, setrecursionlimit
+from heapq import heapify, heappop, heappushpop, heappush
+
+# setrecursionlimit(1 << 30)
+# threading.stack_size(1 << 27)  # Uncomment if recursion depth needed
+
+input = lambda: sys.stdin.readline().strip()
+number = lambda: int(sys.stdin.readline().strip())
+numbers = lambda: map(int, sys.stdin.readline().strip().split())
+string = lambda: sys.stdin.readline().strip()
+strings = lambda: input().split()
+array = lambda: list(map(int, sys.stdin.readline().strip().split()))
+array_set = lambda: set(map(int, sys.stdin.readline().strip().split()))
+yn = lambda x: 'YES' if x else 'NO'
+
+
+def solve():
+    m, n = numbers()
+    a = array()
+    b = array()
+    temp = 0
+    for i in range(1, m):
+        temp = math.gcd(temp, a[i]-a[0])
+    
+    res = [math.gcd(temp, bi + a[0]) for bi in b]
+    print(*res)
+
+def main():
+    t = 1
+    # t = number()
+    for _ in range(t):
+        solve()
+
+
+main()
+#if __name__ == "__main__":
+#    threading.Thread(target=main).start()
