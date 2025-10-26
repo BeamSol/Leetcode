9c111178f006d0b3c2ef1d262e0b1ebca853e705
Beamlak Solomon <141314118+BeamSol@users.noreply.github.com>
2025-10-26 16:23:51 +0300

    Add solution for Design Underground System

---
+++ LeetCode/Design-Underground-System/Solution.py
@@ -0,0 +1,24 @@
+class UndergroundSystem:
+
+    def __init__(self):
+        self.dic = defaultdict(list)
+        self.checkin = defaultdict(int)
+        self.checkout = defaultdict(int)
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.checkin[id] = (stationName, t)
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        name, time = self.checkin[id]
+        self.dic[(name, stationName)].append(t-time)
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        temp = self.dic[(startStation, endStation)] 
+        return sum(temp)/len(temp)
+
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
