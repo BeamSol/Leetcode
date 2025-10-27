6958fdcbe69421df7af3b1f19259fd9adb5d95b6
Beamlak Solomon <141314118+BeamSol@users.noreply.github.com>
2025-10-27 21:07:02 +0300

    Add solution for Number of Laser Beams in a Bank

---
+++ LeetCode/Number-of-Laser-Beams-in-a-Bank/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        ans = 0
+        temp = 0
+        for i in range(1, len(bank)):
+            c1 = bank[i-1].count("1")
+            c2 = bank[i].count("1")
+            if c1 == 0 and c2 == 0:
+                continue
+            elif c2 == 0:
+                ans += temp*c2
+                temp = c1
+            elif c1 == 0:
+                ans += temp*c2
+                temp = c2
+            else:
+                ans += c1*c2
+        return ans
