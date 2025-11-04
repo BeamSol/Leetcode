b6a1eca0679d03dca29708205b44b93db3b6896a
Beamlak Solomon <141314118+BeamSol@users.noreply.github.com>
2025-11-04 21:12:21 +0300

    Created 'Make Sum Divisible by P' by A2SV Hub

---
+++ 04-Nov-2025/Make Sum Divisible by P 407414.py
@@ -0,0 +1,19 @@
+# Problem: Make Sum Divisible by P - https://leetcode.com/problems/make-sum-divisible-by-p/
+
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        prefix = [0]
+        n = len(nums)
+        s = sum(nums) % p
+        if s == 0:
+            return 0
+        for i in range(n):
+            prefix.append((prefix[-1] + nums[i]) % p)
+
+        dic = {0:-1}
+        ans = n
+        for i in range(n+1):
+            if (prefix[i] - s) % p in dic:
+                ans = min(ans, i - dic[(prefix[i] - s) % p])
+            dic[prefix[i]] = i
+        return ans if ans < n else -1
