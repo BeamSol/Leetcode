e7633f364eb80f23d80c591e46cf090e74036c9e
Beamlak Solomon <141314118+BeamSol@users.noreply.github.com>
2025-11-05 14:34:47 +0300

    Add solution for Find Median from Data Stream

---
+++ LeetCode/Find-Median-from-Data-Stream/Solution.py
@@ -0,0 +1,18 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.low = [] 
+        self.high = [] 
+
+    def addNum(self, num: int) -> None:
+        heappush(self.low, -num)
+        heappush(self.high, -heappop(self.low))
+
+        if len(self.low) < len(self.high):
+            heappush(self.low, -heappop(self.high))
+
+    def findMedian(self) -> float:
+        if len(self.low) > len(self.high):
+            return -self.low[0]
+        else:
+            return (-self.low[0] + self.high[0]) / 2
