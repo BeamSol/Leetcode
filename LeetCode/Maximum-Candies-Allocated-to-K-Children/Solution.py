c3dd791142e9d69ede5139970c86c011142d55cf
Beamlak Solomon <141314118+BeamSol@users.noreply.github.com>
2025-11-11 19:04:34 +0300

    Add solution for Maximum Candies Allocated to K Children

---
+++ LeetCode/Maximum-Candies-Allocated-to-K-Children/Solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        def check(mid):
+
+            count = 0
+            for i in range(len(candies)):
+                curr = candies[i]
+                count += curr // mid 
+                if count >= k:
+                    return True
+            return False
+                
+        
+        low = 1
+        high = sum(candies)//k
+
+        while low <= high:
+            mid = (low + high) // 2
+            if check(mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return high
+
+
+        
