fdb6bf393c651c32e8ab184906d182a04fb9512f
Beamlak Solomon <141314118+BeamSol@users.noreply.github.com>
2025-11-13 16:56:44 +0300

    Add solution for Maximum Number of Operations to Move Ones to the End

--- LeetCode/Maximum-Number-of-Operations-to-Move-Ones-to-the-End/Solution.py
+++ LeetCode/Maximum-Number-of-Operations-to-Move-Ones-to-the-End/Solution.py
@@ -1,20 +1,11 @@
 class Solution:
     def maxOperations(self, s: str) -> int:
-        s_ = ""
-        for i in range(len(s)):
-            if s[i] == "1":
-                s_+= "1"
-            elif s_ and s[i] == "0" and s_[-1] == "0":
-                continue
-            else:
-                s_ += "0"
-        zero = s_.count("0")
-        # print(s_)
-        # print(zero)
-        temp = 0
         ans = 0
-        for i in range(len(s_)):
-            if s_[i] == "0":
-                ans += (i-temp)
-                temp += 1
+        ones = 0
+        for i in range(len(s)-1):
+            if s[i] == "1":
+                ones += 1
+            
+                if s[i+1] == "0":
+                    ans += ones
         return ans
