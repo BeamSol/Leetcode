500fa994195ea714a3b713d6f908bff06d8ca578
Beamlak Solomon <141314118+BeamSol@users.noreply.github.com>
2025-11-30 20:28:14 +0300

    Add solution for Make Sum Divisible by P

--- LeetCode/Make-Sum-Divisible-by-P/Solution.py
+++ LeetCode/Make-Sum-Divisible-by-P/Solution.py
@@ -1,17 +1,17 @@
-class Solution:
-    def minSubarray(self, nums: List[int], p: int) -> int:
-        prefix = [0]
-        n = len(nums)
-        s = sum(nums) % p
-        if s == 0:
-            return 0
-        for i in range(n):
-            prefix.append((prefix[-1] + nums[i]) % p)
-
-        dic = {0:-1}
-        ans = n
-        for i in range(n+1):
-            if (prefix[i] - s) % p in dic:
-                ans = min(ans, i - dic[(prefix[i] - s) % p])
-            dic[prefix[i]] = i
-        return ans if ans < n else -1
+1class Solution:
+2    def minSubarray(self, nums: List[int], p: int) -> int:
+3        prefix = [0]
+4        n = len(nums)
+5        s = sum(nums) % p
+6        if s == 0:
+7            return 0
+8        for i in range(n):
+9            prefix.append((prefix[-1] + nums[i]) % p)
+10
+11        dic = {0:-1}
+12        ans = n
+13        for i in range(n+1):
+14            if (prefix[i] - s) % p in dic:
+15                ans = min(ans, i - dic[(prefix[i] - s) % p])
+16            dic[prefix[i]] = i
+17        return ans if ans < n else -1
